5ae8cf16ffb74e6da84c8921ee89f9e9138a23ac
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-08-22 10:49:40 -0700

    Grid

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,10 +6,29 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private Grid grid;
+    [SerializeField]
+    private Vector2 pos;
     public Health health;
 
     private void Awake()
     {
         health = new Health(10);
+    }
+
+    private void Start()
+    {
+        transform.position = grid.LocalToWorld(pos);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void Pathfinding()
+    {
+
     }
 }
