3345874663b3adef573aca8dfd5c2b1520d1c749
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-08-22 12:20:28 -0700

    Path Script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,69 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float speed;
-    [SerializeField]
-    private Grid grid;
     public Health health;
-
-    [SerializeField]
-    private Vector3 pos;
-    [SerializeField]
-    private Vector3 gridPos;
-    public bool temp;
-    public Vector3 cellSize;
 
     private void Awake()
     {
         health = new Health(10);
     }
 
-    private void Start()
-    {
-        cellSize = grid.cellSize;
-        gridPos = pos;
-    }
-
     private void Update()
     {
-        transform.position = grid.LocalToWorld(pos);
-        if (CheckColisions(gridPos))
-        {
-            Debug.Log("Access");
-        }
+
     }
 
-    private void Pathfinding()
-    {
-        
-    }
-
-    private bool CheckColisions(Vector3 pos)
-    {
-        Vector3 cellCenter = pos + cellSize * 0.5f;
-        cellSize.y = 0.0f;
-        Collider[] collider = Physics.OverlapBox(cellCenter, cellSize * 0.5f);
-
-        foreach (var coll in collider)
-        {
-            if (coll.tag == "Player")
-            {
-                temp = true;
-                return true;
-            }
-        }
-        temp = false;
-        return false;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Vector3 cellCeter = gridPos + cellSize * 0.5f;
-        cellSize.y = 0.0f;
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(cellCeter, cellSize);
-    }
 }
---
+++ Assets/Scripts/Enemy/PathFinding.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding
+{
+    [SerializeField]
+    private Vector3 gridPos;
+    private Vector3 cellCenter;
+    private Vector3 cellSize;
+
+    private List<Node> openList;
+
+    private void Pathfinding(Vector3 start, Vector3 end)
+    {
+        openList.Add(new Node(start));
+
+    }
+
+    private bool CheckColisions(Vector3 pos)
+    {
+        cellCenter = pos + cellSize * 0.5f;
+        Collider[] collider = Physics.OverlapBox(cellCenter, cellSize * 0.5f);
+
+        foreach (var coll in collider)
+        {
+            if (coll.tag != "Enemy" || coll.tag != "Ground")
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Vector3 cellCeter = gridPos + cellSize * 0.5f;
+    //    cellSize.y = 0.0f;
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawWireCube(cellCeter, cellSize);
+    //}
+}
+
+public class Node
+{
+    public Node(Vector3 pos) { value = pos; opened = false; }
+
+    public Vector3 value;
+    public bool opened;
+    public Node parent;
+}
+
+public class NodeGrid
+{
+    int _width, _height;
+    Node[] _data;
+
+    bool CheckBounds(int x, int y)
+    {
+        return (x * y < _width * _height);
+    }
+    public NodeGrid(int width, int height)
+    {
+        _data = new Node[width * height];
+        _width = width;
+        _height = height;
+    }
+
+    public NodeGrid()
+    {
+        _width = 0;
+        _height = 0;
+        _data = null;
+    }
+
+    public Node Get(int x, int y)
+    {
+        if (!CheckBounds(x, y))
+            return null;
+
+        return _data[y * _width + x];
+    }
+    public Node Get(int index)
+    {
+        if (index >= _data.Length)
+            return null;
+
+        return _data[index];
+    }
+
+    public void Set(int x, int y, Node data)
+    {
+        if (!CheckBounds(x, y))
+            return;
+
+        _data[y * _width + x] = data;
+    }
+    public void Set(int index, Node data)
+    {
+        if (index >= _data.Length)
+            return;
+
+        _data[index] = data;
+    }
+
+}
