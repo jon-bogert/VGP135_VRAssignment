b3a1d66525f5fcbb3feb730b8defa9dfd7b21065
Jon Bogert <jonbogert@gmail.com>
2023-09-11 14:21:11 -0700

    MainMenu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,56 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public delegate void ScoreEvent(ulong score);
 
 public class GameManager : MonoBehaviour
 {
+    ulong score = 0;
+    ulong highScore = 0;
+
+    public ulong Score { get { return score; } }
+    public ulong HighScore { get { return highScore; } }
+
+    public ScoreEvent scoreAdded;
+
     private void Awake()
     {
+        if (FindObjectsOfType<GameManager>().Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         Application.targetFrameRate = 120;
+    }
+
+    public void AddToScore(uint amt)
+    {
+        score += amt;
+        scoreAdded?.Invoke(score);
+    }
+
+    public void GameOver()
+    {
+        if (score > highScore)
+            highScore = score;
+
+        score = 0;
+        LoadGameOver();
+    }
+
+    private void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    TMP_Text scoreText;
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        if (!scoreText)
+            Debug.LogError("ScoreText -> Could not find Text Component");
+    }
+
+    private void Start()
+    {
+        gameManager  = FindObjectOfType<GameManager>();
+        if (!gameManager)
+            Debug.LogError("ScoreText->Could not find GameManager in Scene");
+
+        gameManager.scoreAdded += OnScoreUpdate;
+        OnScoreUpdate(gameManager.Score);
+    }
+
+    private void OnDestroy()
+    {
+        gameManager.scoreAdded -= OnScoreUpdate;
+    }
+
+    void OnScoreUpdate(ulong score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
